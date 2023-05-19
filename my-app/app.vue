7bc8beb1b33d4758181dbbc53a9717c7818a008a
marwan-elabhar <marwan.a.elabhar@gmail.com>
2023-05-19 18:56:44 +0300

    add npm package

---
+++ my-app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-app-gray-200">
+    <p class="text-app-success-500">hello</p>
+  </div>
+</template>
---
+++ my-app/nuxt.config.js
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss',
+      ],
+    extends: '@marwan-elabhar/my-nuxt-layer',
+
+})
